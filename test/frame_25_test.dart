cf2d99abae2911a2f4a8b6b096638086a80ac990
Mangirdas Kazlauskas <mangirdas@billo.app>
2024-11-01 21:43:31 +0200

    test: add assertion error test

--- test/frame_25_test.dart
+++ test/frame_25_test.dart
@@ -4,6 +4,25 @@ import 'package:frame_25/frame_25.dart';
 
 void main() {
   group('Frame25', () {
+    group('when maxDelayInMilliseconds is <= 0', () {
+      testWidgets('should throw assertion error', (tester) async {
+        expect(
+          () => Frame25.random(
+            maxDelayInMilliseconds: 0,
+            frame25: const Text(''),
+            child: const SizedBox.shrink(),
+          ),
+          throwsA(
+            isA<AssertionError>().having(
+              (e) => e.message,
+              'message',
+              'maxDelayInMilliseconds must be greater than 0',
+            ),
+          ),
+        );
+      });
+    });
+
     testWidgets('should show the 25th frame', (tester) async {
       const delay = Duration(seconds: 1);
       final frame25Delay = Duration(milliseconds: (1 / 24 * 1000).round());
